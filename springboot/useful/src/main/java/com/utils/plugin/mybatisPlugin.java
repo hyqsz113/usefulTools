7a8e3c696519a0a186f3091b4e2d202a2c48a28b
hyq <heyaqi@zywamail.com>
2020-06-28 14:22:27 +0800

    fix: mybatis插件类修改

--- springboot/useful/src/main/java/com/utils/plugin/mybatisPlugin.java
+++ springboot/useful/src/main/java/com/utils/plugin/mybatisPlugin.java
@@ -1,23 +1,37 @@
 package com.utils.plugin;
 
 import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
 
 import java.sql.Connection;
 import java.util.Properties;
 
 /**
- * @description: @intercepts注解就是用来标明拦截哪些类，哪些方法
+ * @description:
  * @program: usefulTools
  * @author: hyq
  * @create: 2020-06-28 10:51
  **/
+
+/**
+ *  @Intercepts 注解就是用来标明拦截哪些类，哪些方法
+ *
+ * 总结，mybatis可以拦截的类分四大类
+ *
+ * 一、executor，executor类可以说是执行sql的全过程，如组装参数，sql改造，结果处理，比较广泛，但实际用的不多
+ *
+ * 二、StatementHandler，这个是执行sql的过程，可以获取到待执行的sql，可用来改造sql，如分页，分表，最常拦截的类
+ *
+ * 三、paremeterHandler，这个用来拦截sql的参数，可以自定义参数组装规则
+ *
+ * 四、resultHandler，这个用来处理结果
+ */
 @Intercepts(
         value = {
-                @Signature(
-                        type = StatementHandler.class,
-                        method = "prepare",
-                        args = {Connection.class}
+                @Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class}
                 )
         }
 )
@@ -32,6 +46,20 @@ public class mybatisPlugin implements Interceptor {
      */
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        MetaObject metaObject = SystemMetaObject.forObject(statementHandler);
+
+        BoundSql boundSql = (BoundSql) metaObject.getValue("delegate.boundSql");
+
+        String sql = boundSql.getSql();
+        System.out.println("===");
+        System.out.println("===");
+        System.out.println("拦截了"+sql);
+        System.out.println("===");
+        System.out.println("===");
+        // 设置 sql 语句
+        metaObject.setValue("",sql);
+
         return invocation.proceed();
     }
 
